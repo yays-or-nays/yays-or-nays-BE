d0f03a514a4007fd3c3400638f9c3e74a9df4c05
Sam Yeo <yeosamuel95@gmail.com>
2021-04-09 16:59:07 -0600

    Add test to tag model
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Relationship' do
+    it { should have_many :hot_takes }
+  end
 end
