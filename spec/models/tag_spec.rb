4480b8666a42b94201798598a39e847bd29477c4
Sam Yeo <yeosamuel95@gmail.com>
2021-04-09 17:34:56 -0600

    Create validation spec
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Tag, type: :model do
   describe 'Relationship' do
     it { should have_many :hot_takes }
   end
+  describe 'Validation' do
+    it { validate_presence_of :category }
+  end
 end
