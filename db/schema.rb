fce41d813317db9e7beb0fb4ca98a5fa343efc34
Sam Yeo <yeosamuel95@gmail.com>
2021-04-09 16:25:07 -0600

    Create tagged takes migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_143000) do
+ActiveRecord::Schema.define(version: 2021_04_09_222339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,19 @@ ActiveRecord::Schema.define(version: 2021_04_09_143000) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tagged_takes", force: :cascade do |t|
+    t.bigint "hot_take_id"
+    t.bigint "tag_id"
+    t.index ["hot_take_id"], name: "index_tagged_takes_on_hot_take_id"
+    t.index ["tag_id"], name: "index_tagged_takes_on_tag_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tagged_takes", "hot_takes"
+  add_foreign_key "tagged_takes", "tags"
 end
