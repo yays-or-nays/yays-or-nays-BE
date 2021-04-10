2bc93b4c858fa9d8088ac05d5c07482a153f68a3
Isabelle Villasenor <villasenori@outlook.com>
2021-04-10 12:49:37 -0600

    Fix typo
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- app/graphql/types/tag_type.rb
+++ app/graphql/types/tag_type.rb
@@ -2,5 +2,10 @@ module Types
   class TagType < Types::BaseObject
     field :id, ID, null: false
     field :category, String, null: false
+    field :hot_takes, [HotTakeType], null: false
+
+    def hot_takes
+      AssociationLoader.for(object.class, :hot_takes).load(object)
+    end
   end
 end
