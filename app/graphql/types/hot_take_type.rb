639cedaf420a3f42dcdc631e4163e4a2d4a507f4
Isabelle Villasenor <villasenori@outlook.com>
2021-04-10 13:49:22 -0600

    Remove Unnecessary Methods
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- app/graphql/types/hot_take_type.rb
+++ app/graphql/types/hot_take_type.rb
@@ -6,14 +6,6 @@ module Types
     field :yes_vote, Integer, null: false
     field :no_vote, Integer, null: false
     field :tag_id, ID, null: false
-    #field :tag, [Tag], null: false
     field :tag, TagType, null: false
-
-    # def yes_vote_count
-    #   object.yes_vote
-    # end
-    def tag
-      RecordLoader.for(Tag).load(object.tag_id)
-    end
   end
 end
