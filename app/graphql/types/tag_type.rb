24ed1ad7e8babb34a5de3e3cd3bbee9027cab91c
Isabelle Villasenor <villasenori@outlook.com>
2021-04-10 13:50:24 -0600

    Remove Unnecessary Methods
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- app/graphql/types/tag_type.rb
+++ app/graphql/types/tag_type.rb
@@ -3,9 +3,5 @@ module Types
     field :id, ID, null: false
     field :category, String, null: false
     field :hot_takes, [HotTakeType], null: false
-
-    def hot_takes
-      AssociationLoader.for(object.class, :hot_takes).load(object)
-    end
   end
 end
