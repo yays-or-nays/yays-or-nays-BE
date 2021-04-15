639ca3ebdcfaf1205797bed60ae4805ea66286fc
Saundra Catalina <saundracatalina@gmail.com>
2021-04-15 16:10:32 -0600

    Add mutation files to update votes
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,13 @@
 module Types
   class MutationType < Types::BaseObject
+    # field :update_vote, mutation: Mutations::HotTake::UpdateVote
+    # field :update_vote, Integer, mutation::Mutations::UpdateVote
+    field :update_vote, Integer, null: false
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World"
+    # end
   end
 end
