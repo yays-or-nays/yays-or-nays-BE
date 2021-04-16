e8a83393d368191f5e3985229fefd7a8c0be44a7
Sam Yeo <yeosamuel95@gmail.com>
2021-04-16 10:16:37 -0600

    Add require statement due to autoload issues
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,8 +1,8 @@
+require 'mutations/base_mutation'
+
 module Types
   class MutationType < Types::BaseObject
-    # field :update_vote, mutation: Mutations::HotTake::UpdateVote
-    # field :update_vote, Integer, mutation::Mutations::UpdateVote
-    field :update_vote, Integer, null: false
+    field :increment_yes_vote, mutation: Mutations::IncrementYesVote
     # TODO: remove me
     # field :test_field, String, null: false,
     #   description: "An example field added by the generator"
