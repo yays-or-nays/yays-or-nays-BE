95ac4cf62dc2a1448a2f3b1040fee320f10c3fb7
Sam Yeo <yeosamuel95@gmail.com>
2021-04-16 11:54:39 -0600

    Fix typo for seed files
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,7 @@ software.hot_takes.create!(picture: "2", question: "Javascript is a good coding 
 software.hot_takes.create(picture: "1", question: "Ruby is a great beginner coding language", yes_vote: 3)
 software.hot_takes.create(picture: "1", question: "Javascript has too many unecessary brackets", yes_vote: 2)
 software.hot_takes.create(picture: "1", question: "If you use VS Code and don't use the built in terminal it's weird", yes_vote: 1, no_vote: 1)
+software.hot_takes.create(picture: "1", question: "Light mode on any application is terrible", yes_vote: 1, no_vote: 2)
 
 food_tag.hot_takes.create(picture: "1", question: "IPAs taste like bile", yes_vote: 1)
 food_tag.hot_takes.create(picture: "1", question: "Boneless wings are better than bone-in wings", yes_vote: 2, no_vote: 3)
@@ -25,14 +26,13 @@ food_tag.hot_takes.create(picture: "1", question: "Whiteclaw does the job and sh
 food_tag.hot_takes.create(picture: "1", question: "Connecticut is the pizza capital of the world", yes_vote: 2)
 food_tag.hot_takes.create(picture: "1", question: "Angel Hair is the worst pasta", yes_vote: 1, no_vote: 2)
 food_tag.hot_takes.create(picture: "1", question: "Dunkin Donuts > Krispy Kreme", yes_vote: 2, no_vote: 1)
-food_tag.hot_takes.create(picture: "1", question: "Light mode on any application is terrible", yes_vote: 1, no_vote: 2)
 food_tag.hot_takes.create(picture: "1", question: "Whataburger and In n Out are the same thing", yes_vote: 1, no_vote: 2)
-food_tag.hot_takes.create(picture: "1", question: "Most songs would be better without the flute", yes_vote: 1, no_vote: 2)
 food_tag.hot_takes.create(picture: "1", question: "Tomatoes are a drink", no_vote: 2)
 food_tag.hot_takes.create(picture: "1", question: "Cinnamon is better in savory foods", yes_vote: 1)
 food_tag.hot_takes.create(picture: "1", question: "Pizza is overrated", yes_vote: 1)
 food_tag.hot_takes.create(picture: "1", question: "Soup is a drink", yes_vote: 1, no_vote: 2)
 
+entertainment_tag.hot_takes.create(picture: "1", question: "Most songs would be better without the flute", yes_vote: 1, no_vote: 2)
 entertainment_tag.hot_takes.create(picture: "1", question: "The Queen's Gambit Needs More Chess", yes_vote: 1)
 entertainment_tag.hot_takes.create(picture: "1", question: "The Chimera ant arc in HxH is not only the best arc of the series, but possibly the best of any anime/manga ever, period.", yes_vote: 1, no_vote: 2)
 entertainment_tag.hot_takes.create(picture: "1", question: "The Empire did nothing wrong.", yes_vote: 1, no_vote: 1)
