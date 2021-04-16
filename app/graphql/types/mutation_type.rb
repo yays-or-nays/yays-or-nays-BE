896d64cfb3054aeec725374cb37912044f320cd4
Sam Yeo <yeosamuel95@gmail.com>
2021-04-16 10:30:35 -0600

    Remove commented sample method
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,11 +3,6 @@ require 'mutations/base_mutation'
 module Types
   class MutationType < Types::BaseObject
     field :increment_yes_vote, mutation: Mutations::IncrementYesVote
-    # TODO: remove me
-    # field :test_field, String, null: false,
-    #   description: "An example field added by the generator"
-    # def test_field
-    #   "Hello World"
-    # end
+    field :increment_no_vote, mutation: Mutations::IncrementNoVote
   end
 end
