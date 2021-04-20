5366f8c967964514ea8b0dac1f65ac4cc6d44eb6
Sam Yeo <yeosamuel95@gmail.com>
2021-04-19 19:02:43 -0600

    :white_check_mark: Complete first query spec for GraphQL by testing Tags query
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- spec/requests/category_query_spec.rb
+++ spec/requests/category_query_spec.rb
@@ -1,40 +1,37 @@
 require 'rails_helper'
 
 describe 'receive fe request' do
-  xit 'receive fe request for all categories & return response with all' do
-    # post "/graphql", params:'query{
-    #                           tags{
-    #                             category
-    #                             }
-    #                           }'
+  it 'receive fe request for all categories & return response with all' do
     tag1 = Tag.create!(category: "Food & Beverage")
     tag2 = Tag.create!(category: "Entertainment")
     tag3 = Tag.create!(category: "Software Development")
-    post "/graphql", params: { query: query(category: tag1.category) }
 
-    json_response = {
-                "data": {
-                  "tags": [
+    headers = { 'Content_Type': 'application/json' }
+    
+    post '/graphql', headers: headers, params: { "query": "{tags{category}}"}
+
+    
+    expected_response = {
+                data: {
+                  tags: [
                     {
-                      "category": "Food & Beverage"
+                      category: "Food & Beverage"
                     },
                     {
-                      "category": "Entertainment"
+                      category: "Entertainment"
                     },
                     {
-                      "category": "Software Development"
+                      category: "Software Development"
                     }
                   ]
                 }
               }
-    categories = json_response[:data][:tags]
-    require "pry"; binding.pry
-    expect(response.status).to eq(200)
-    # expect(response.body)to eq(categories[0][:category])
+    categories = expected_response[:data][:tags]
+              
+    json_response = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    # expect(categories[0][:category]).to eq("Food & Beverage")
-    # expect(categories[1][:category]).to eq("Entertainment")
-    # expect(categories[2][:category]).to eq("Software Development")
-    # post "/widgets", params: '{ "widget": { "name":"My Widget" } }'
+    expect(response.status).to eq(200)
+ 
+    expect(categories).to eq(json_response[:tags])
   end
 end
