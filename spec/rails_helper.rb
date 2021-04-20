fa26919c446b797a7a360f9fd3475831bfd7c44a
Sam Yeo <yeosamuel95@gmail.com>
2021-04-20 17:16:47 -0600

    🔧 Remove certain GraphQL development based code from SimpleCov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter "app/controllers/graphql_controller"
+  add_filter "config/routes"
+end
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
